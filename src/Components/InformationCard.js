b9510916d61ab5343f614ca5a2e299977fbac22d
John leo <johntoluwalase80@gmail.com>
2026-02-03 20:52:09 +0100

    Extend Pulse-and-Fill animation to InformationCard components

--- src/Components/InformationCard.js
+++ src/Components/InformationCard.js
@@ -4,16 +4,13 @@ import { motion } from "framer-motion";
 
 function InformationCard(props) {
   return (
-    <motion.div
-      className="info-cards"
-      whileHover={{ y: -10, transition: { duration: 0.3 } }}
-    >
-      <span className="info-card-icon">
+    <div className="info-cards">
+      <div className="info-card-icon-wrapper">
         <FontAwesomeIcon className="info-fa-icon" icon={props.icon} />
-      </span>
+      </div>
       <p className="info-card-title">{props.title}</p>
       <p className="info-card-description">{props.description}</p>
-    </motion.div>
+    </div>
   );
 }
 
