7b3437ea9ee371ad626c64e170afebc7a13d5ba1
John leo <johntoluwalase80@gmail.com>
2026-02-08 01:19:45 +0100

    finaly it would work

--- server/index.js
+++ server/index.js
@@ -7,7 +7,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://albright-clinic-ckzs.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://www.xnetnigeria.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Debug: Log all requests
